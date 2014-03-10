f5d3d1b86150cc502f2954b450ab661b8ff9f976
jwarmkessel <justin@goosii.com>
2014-03-10 16:49:48 -0700

    First commit

---
+++ VMWareBlogs/VMBlogFeedViewController.h
@@ -0,0 +1,15 @@
+//
+//  VMBlogFeedViewController.h
+//  VMWareBlogs
+//
+//  Created by Justin Warmkessel on 3/10/14.
+//  Copyright (c) 2014 Justin Warmkessel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VMBlogFeedViewController : UITableViewController
+
+@property (nonatomic,strong) NSManagedObjectContext* managedObjectContext;
+
+@end
