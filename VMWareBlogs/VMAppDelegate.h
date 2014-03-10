9ca0383d246b9cd3adaa898a9939215f6daf233f
jwarmkessel <justin@goosii.com>
2014-03-10 15:08:10 -0700

    Initial Commit

---
+++ VMWareBlogs/VMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VMAppDelegate.h
+//  VMWareBlogs
+//
+//  Created by Justin Warmkessel on 3/10/14.
+//  Copyright (c) 2014 Justin Warmkessel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VMWareBlogs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VMWareBlogs
+//
+//  Created by Justin Warmkessel on 3/10/14.
+//  Copyright (c) 2014 Justin Warmkessel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VMAppDelegate class]));
+    }
+}
